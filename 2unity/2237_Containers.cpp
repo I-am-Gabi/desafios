69c7da8ac2b07aae66ce0e2c232293dc97515824
I-am-Gabi <gabicavalcantesilva@gmail.com>
2016-09-27 02:19:27 -0300

    segunda versao com manipulacao de bits

--- 2unity/2237_Containers.cpp
+++ 2unity/2237_Containers.cpp
@@ -11,38 +11,51 @@
  *
 1 2 3 4
 5 6 7 8
-1 2 3 4
-6 5 7 8
+0 1 2 3
+5 4 6 7
+
  */
 using namespace std;
-#define NUMBER_SIZE 10
+#define WORD 24
+#define BLOCK 3
 #define NEIGHBORS 8
 
 int min_weight = 0;
-vector<bitset<NUMBER_SIZE>> final_containers;
+vector<bitset<10>> final_containers;
+vector<bitset<10>> initial_containers;
+
+int index_map[8];
 
 struct container {
-    vector<bitset<NUMBER_SIZE>> slots;
+    bitset<WORD> slots;
     int weight;
 };
 bool operator < (const container &l, const container &r) { return l.weight < r.weight; }
 
-void print(vector<bitset<NUMBER_SIZE>> ship) {
-  unsigned long n_containers = 0;
-  while(n_containers < NEIGHBORS) {
-    printf("%d ", ship[n_containers++]);
-    if (n_containers == 4) printf("\n");
-  }
-  printf("\n\n");
+bitset<WORD> set(int position, bitset<WORD> value, bitset<WORD> word) {
+  bitset<WORD> default_value(7);
+
+  value <<= position * BLOCK;
+  default_value <<= position * BLOCK;
+
+  word = ((~default_value)&word);
+  return (value|word);
+}
+
+bitset<WORD> getBit(bitset<WORD> byte, int pos) {
+  return (((byte >> (pos * BLOCK)) << (7 * BLOCK)) >> (7 * BLOCK));
 }
 
 container switch_containers(container s, int origin, int destination){
   container new_ship = s;
-  bitset<NUMBER_SIZE> tmp = new_ship.slots[destination];
-  new_ship.slots[destination] = new_ship.slots[origin];
-  new_ship.slots[origin] = tmp;
-  int w = (int) (new_ship.slots[destination].to_ulong() + new_ship.slots[origin].to_ulong());
+  int o = getBit(new_ship.slots, origin).to_ulong();
+  int d = getBit(new_ship.slots, destination).to_ulong();
+
+  new_ship.slots = set(origin, d, new_ship.slots);
+  new_ship.slots = set(destination, o, new_ship.slots);
+  int w = index_map[getBit(new_ship.slots, destination).to_ulong()] + index_map[getBit(new_ship.slots, origin).to_ulong()];
   new_ship.weight += w;
+
   return new_ship;
 }
 
@@ -59,20 +72,39 @@ map<container, vector<container>> permute(map<container, vector<container> > pat
   return path_map;
 }
 
-bool compare(vector<bitset<NUMBER_SIZE>> slot) {
+
+void print(vector<bitset<10>> ship) {
+  unsigned long n_containers = 0;
+  while(n_containers < NEIGHBORS) {
+    printf("%d ", ship[n_containers++]);
+    if (n_containers == 4) printf("\n");
+  }
+  printf("\n\n");
+}
+
+void print(bitset<WORD> ship) {
+  int n_containers = 0;
+  while(n_containers < NEIGHBORS) {
+    cout << getBit(ship, n_containers++).to_ulong() << ' ';
+    if (n_containers == 4) printf("\n");
+  }
+  printf("\n\n");
+}
+
+bool compare(bitset<WORD> slot) {
   int n_containers = 0;
 
-  while(n_containers < NEIGHBORS)
-    if (slot[n_containers] != final_containers[n_containers++]) return false;
-
+  while(n_containers < NEIGHBORS) {
+    if (getBit(slot, n_containers).to_ulong() != final_containers[n_containers++].to_ulong()) return false;
+  }
   return true;
 }
 
-void process(container current_ship) {
+void process(container c){
   map<container, vector<container> > path_map;
   queue<container> queue;
 
-  queue.push(current_ship);
+  queue.push(c);
   int cont = 0;
 
   while(!queue.empty()) {
@@ -93,13 +125,9 @@ void process(container current_ship) {
     path_map = permute(path_map, c);
     vector<container> adjacent_list = path_map[c];
 
-
-
     int comp = compare(c.slots);
     if (comp && (w < min_weight || min_weight == 0)) {
       min_weight = w;
-      printf("weight: %d \n", c.weight);
-      print(c.slots);
       continue;
     }
 
@@ -108,36 +136,47 @@ void process(container current_ship) {
       queue.push(v);
     }
   }
-
-  return;
 }
+
+void create_map() {
+  for (int i = 0; i < NEIGHBORS; i++) {
+    index_map[i] = (int) initial_containers[i].to_ulong();
+  }
+}
+
 
 int main() {
   container s;
-  s.slots = vector<bitset<NUMBER_SIZE>>();
-  final_containers = vector<bitset<NUMBER_SIZE>>();
+  s.slots = bitset<WORD>();
 
+  for (int i = 0; i < NEIGHBORS; i++) {
+    bitset<WORD> value(i);
+    s.slots = set(i, value, s.slots);
+  }
 
   unsigned long n_containers = 0;
 
   while(n_containers++ < NEIGHBORS) {
     int container;
     scanf("%d", &container);
-    bitset<NUMBER_SIZE> b(container);
-    s.slots.push_back(b);
+    bitset<10> b(container);
+    initial_containers.push_back(b);
   }
+  create_map();
 
   n_containers = 0;
   while(n_containers++ < NEIGHBORS) {
     int container;
     scanf("%d", &container);
-    bitset<NUMBER_SIZE> b(container);
+    bitset<10> b(container);
     final_containers.push_back(b);
   }
 
   process(s);
 
-  printf("%d", min_weight);
+  printf("-- %d", min_weight);
 
   return 0;
 }
+
+
